300dd04d7348295201d186605d9359a405549230
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-09-03 05:06:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "omarosman23"
 
     workspaces {
-      name = "juno-node"
+      name = "gh-actions-demo"
     }
   }
 
@@ -17,11 +17,13 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
+  access_key="AKIAXWXEZ2EG2JWC6QFI"
+  secret_key="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"
 }
 
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+resource "aws_key_pair" "key-module" {
+  key_name   = "key-module"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqIeoyluNk/fW9w/7gBoOW3bCZrZf2f4dhPgWtGbJ/HukKCyzkxa2ugKlQWkTmAYX848UI6onhWF9Py42kiF7XVUiTXxHto1KSBmW1jM7vFK2zF/c/qcLIpsOfOh9kIMXLnk009mloMCtzGByCSKcBG63RhEdQ+Zqkxifm+2R6E6ryLauQouDXRYYr/TXqZmFFmftNhABXJKWWbAtkyBkm86pO2L1E+GxGcKoi7jjSi0wwWKocQEDL1akHrlWh4r8iBTeei0ycO38bYlnbSr3mIQCdIEifWbgVFbJ5CrPsEVI1OJQy987ZhtwJcE3eYTowzLYcr+jFYMOd8RdiBcYr"
 }
 
@@ -34,7 +36,7 @@ resource "aws_instance" "web" {
   
   ami           = "ami-0d70546e43a941d70"
   instance_type = "t2.large"
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name = "${aws_key_pair.key-module.key_name}"
   user_data = data.template_file.user_data.rendered
   vpc_security_group_ids = [aws_security_group.sg_web.id]
 
